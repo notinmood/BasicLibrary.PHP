9a73a15d6e7017056aae903581ef236a22e95b29
xiedali <9727005@qq.com>
2018-11-18 20:20:38 +0800

    添加百度小程序的逻辑

---
+++ Biz/Baidu/MiniProgramConfig.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: xiedali
+ * Date: 2018/11/18
+ * Time: 19:22
+ */
+
+namespace Hiland\Biz\Baidu;
+use Hiland\Biz\ThinkAddon\TPCompatibleHelper;
+
+class MiniProgramConfig
+{
+    public static function getAPPID()
+    {
+        $result = TPCompatibleHelper::config("baidu-mimiprogram-appid");
+
+        if (empty($result)) {
+            $result = "14768308";
+        }
+
+        return $result;
+    }
+
+    public static function getAPPKEY()
+    {
+        $result = TPCompatibleHelper::config("baidu-mimiprogram-appkey");
+
+        if (empty($result)) {
+            $result = "zHempCohziKG7AkGZ3kGVDFIbKjyvMgV";
+        }
+
+        return $result;
+    }
+
+    public static function getSECRET()
+    {
+        $result = TPCompatibleHelper::config("baidu-mimiprogram-secret");
+
+        if (empty($result)) {
+            $result = "auIuVIChGE80HNgUU5kjcMp0CFtNMcla";
+        }
+
+        return $result;
+    }
+}
