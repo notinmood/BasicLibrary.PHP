a20aa7130259f9ae117f7af10eddc451da156dc7
xiedali <notinmood@gmail.com>
2021-10-14 11:13:20 +0800

    feature: 将常用的类型中的方法，编排进入函数，便于调用

--- Utils/Common/functions.php
+++ Utils/Common/functions.php
@@ -8,11 +8,29 @@
  * @company: HiLand & RainyTop
  */
 
+/**
+ * 将经常使用到的类型中的方法进行提取出来形成函数,便于使用.
+ */
+
+use Hiland\Utils\Data\ObjectHelper;
 use Hiland\Utils\Data\ThinkHelper;
 
+/**
+ * 将var_dump进行简短表示
+ */
 if (!function_exists('dump') && ThinkHelper::isThinkPHP() == false) {
     function dump($value)
     {
         var_dump($value);
+    }
+}
+
+/**
+ * 判断目标标的是否存在
+ */
+if (!function_exists("exist")) {
+    function exist($value)
+    {
+        return ObjectHelper::isExist($value);
     }
 }
