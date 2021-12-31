75caa36488eef525825471d6882d5839f6b334a5
解大劦 <9727005@qq.com>
2021-12-31 23:40:18 +0800

    feature: 添加数据库的单元测试功能

--- test/database/DatabaseUnitTestTest.php
+++ test/database/DatabaseUnitTestTest.php
@@ -30,18 +30,29 @@ class DatabaseUnitTestTest extends TestCase
         self::assertEquals($expected, $actual);
     }
 
+
+
     public function test_is_exist_table2()
     {
         $table_name = "user";
         $biz = new DatabaseUnitTest($table_name, 2);
 
-        /**
-         * 创建完成biz的时候，数据库内有 DatabaseUnitTest 创建的表 ***_user_***;
-         * 使用完成后，数据库内的表 ***_user_*** 自动销毁。可以手动查看。
-         */
         $new_table_name = $biz->getNewTableName();
         $actual = $biz->getDDL()->isExistTable($new_table_name);
         $expected = true;
         self::assertEquals($expected, $actual);
+
+        return $new_table_name;
+    }
+
+    public function test_is_exist_table3(){
+        /**
+         * test_is_exist_table2创建完成biz的时候，数据库内有 DatabaseUnitTest 创建的表 ***_user_***;
+         * 但，本地函数调用test_is_exist_table2使用完成后，数据库内的表 ***_user_*** 自动销毁。
+         */
+        $newTableName = $this->test_is_exist_table2();
+        $actual =  DatabaseClient::getDDL()->isExistTable($newTableName);
+        $expected = false;
+        self::assertEquals($expected,$actual);
     }
 }
