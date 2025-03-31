51a792333c1f1174d02e6f168134c28bab558e57
解大劦 <9727005@qq.com>
2025-03-31 17:31:08 +0800

    perf: 增强字符串功能类

--- src/Finance/RMB.php
+++ src/Finance/RMB.php
@@ -10,9 +10,9 @@ class RMB
 {
     /**
      * 数字转换为中文
-     * @param string|integer|float $num         目标数字
-     * @param bool                 $financeMode 模式[true:金额（默认）,false:普通数字表示]
-     * @param bool                 $simpleMode  使用简体小写还是繁体大写（默认小写）
+     * @param float|int $num 目标数字
+     * @param bool $financeMode 模式[true:金额（默认）,false:普通数字表示]
+     * @param bool $simpleMode 使用简体小写还是繁体大写（默认小写）
      * @return string
      * @example
      *                                          $num = ’0123648867.789′;
@@ -28,9 +28,12 @@ class RMB
      *                                          ‘;
      *                                          echo ‘金额(繁体):’,displayChineseValue($num,true,false);
      */
-    public static function displayChineseValue($num, bool $financeMode = true, bool $simpleMode = true): string
+    public static function displayChineseValue(float|int $num, bool $financeMode = true, bool $simpleMode = true): string
     {
-        if (!is_numeric($num)) return '含有非数字非小数点字符！';
+        if (!is_numeric($num)) {
+            return '含有非数字非小数点字符！';
+        }
+
         $char   = $simpleMode ? array('零', '一', '二', '三', '四', '五', '六', '七', '八', '九')
             : array('零', '壹', '贰', '叁', '肆', '伍', '陆', '柒', '捌', '玖');
         $unit   = $simpleMode ? array('', '十', '百', '千', '', '万', '亿', '兆')
@@ -39,8 +42,8 @@ class RMB
 
         //小数部分
         if (strpos($num, '.')) {
-            list($num, $dec) = explode('.', $num);
-            $dec = strval(round($dec, 2));
+            [$num, $dec] = explode('.', $num);
+            $dec = (string)round($dec, 2);
             if ($financeMode) {
                 $result .= "{$char[$dec['0']]}角{$char[$dec['1']]}分";
             } else {
@@ -51,21 +54,21 @@ class RMB
         }
 
         //整数部分
-        $str = $financeMode ? strrev(intval($num)) : strrev($num);
+        $str = $financeMode ? strrev((int)$num) : strrev($num);
         $out = null;
         for ($i = 0, $c = strlen($str); $i < $c; $i++) {
             $out[$i] = $char[$str[$i]];
             if ($financeMode) {
-                $out[$i] .= $str[$i] != '0' ? $unit[$i % 4] : '';
-                if ($i > 1 and $str[$i] + $str[$i - 1] == 0) {
+                $out[$i] .= $str[$i] !== '0' ? $unit[$i % 4] : '';
+                if ($i > 1 && $str[$i] + $str[$i - 1] === 0) {
                     $out[$i] = '';
                 }
-                if ($i % 4 == 0) {
+                if ($i % 4 === 0) {
                     $out[$i] .= $unit[4 + floor($i / 4)];
                 }
             }
         }
 
-        return join('', array_reverse($out)) . $result;
+        return implode('', array_reverse($out)) . $result;
     }
 }
